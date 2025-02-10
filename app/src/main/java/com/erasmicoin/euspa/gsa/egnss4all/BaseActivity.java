0bdca39469ee310e1c943242014aceb28d39a5a5
buildvoc10 <phil@buildvoc.co.uk>
2025-02-10 17:10:53 +0000

    compare-changes

--- app/src/main/java/com/erasmicoin/euspa/gsa/egnss4all/BaseActivity.java
+++ app/src/main/java/com/erasmicoin/euspa/gsa/egnss4all/BaseActivity.java
@@ -103,12 +103,13 @@ public abstract class BaseActivity extends AppCompatActivity implements ServiceI
             Manifest.permission.ACCESS_WIFI_STATE,
             Manifest.permission.READ_PHONE_STATE,
             Manifest.permission.CAMERA,
-            Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_ADMIN
+            Manifest.permission.BLUETOOTH_ADMIN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.BLUETOOTH_SCAN,
     };
 
     @Override
